7c0af5bc5b69741f78f6ef9dd53d385e83aa9eb1
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2023-06-12 12:03:27 +0530

    api test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const url = 'https://predictify-backend.vercel.app/predict';
+  const headers = {
+    imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQMeGkHXYMbo_CyhPeNodJXbCCoO3AM-HcQ&usqp=CAU",
+  };
+  axios
+    .post(url, null, { headers })
+    .then((response) => {
+
+      console.log(response.data.predictedText);
+      // Handle the result as needed
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      // Handle the error
+    });
+ 
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Predictify</h1>
+       
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
